9dc413aa722428edc4eb62b769d2bbe7c31f2a57
Beatriz da Costa <beaadc3401@gmail.com>
2022-01-09 15:39:08 +0100

    fixed ext3 i 4 + outputTest més "user friendly"🥴

--- extension3/jp2.pddl
+++ extension3/jp2.pddl
@@ -1,2 +1,83 @@
-(define (problem jp2) (:domain hotel)
+(define (problem jp1) (:domain hotel)
+    (:objects
+    	h1 h2 h3 h4 - habitacion
+    	r1  r2  r3  r4  r5  r6  r7  r8 - reserva
+        r11 r22 r33 r44 r55 r66 r77 r88 - reserva
+        
+    )
+    (:init
+        (= (cantidad_reservas) 0)
+        (= (xctj_ocupacion) 0)
+
+        (= (start_day r1) 1)
+        (= (start_day r2) 1)
+        (= (start_day r3) 4)
+        (= (start_day r4) 4)
+        (= (start_day r5) 8)
+        (= (start_day r6) 8)
+        (= (start_day r7) 12)
+        (= (start_day r8) 12)
+        
+        (= (start_day r11) 1)
+        (= (start_day r22) 1)
+        (= (start_day r33) 4)
+        (= (start_day r44) 4)
+        (= (start_day r55) 8)
+        (= (start_day r66) 8)
+        (= (start_day r77) 12)
+        (= (start_day r88) 12)
+        
+        (= ( end_day r1) 3)
+        (= ( end_day r2) 3)
+        (= ( end_day r3) 7)
+        (= ( end_day r4) 7)
+        (= ( end_day r5) 11)
+        (= ( end_day r6) 11)
+        (= ( end_day r7) 20)
+        (= ( end_day r8) 20)
+        
+        (= ( end_day r11) 3)
+        (= ( end_day r22) 3)
+        (= ( end_day r33) 7)
+        (= ( end_day r44) 7)
+        (= ( end_day r55) 11)
+        (= ( end_day r66) 11)
+        (= ( end_day r77) 20)
+        (= ( end_day r88) 20)
+
+        (= (tamano h1) 1)
+        (= (tamano h2) 2)
+        (= (tamano h3) 3)
+        (= (tamano h4) 4)
+
+        (= (tamano r1) 1)
+        (= (tamano r2) 2)
+        (= (tamano r3) 3)
+        (= (tamano r4) 4)
+        (= (tamano r5) 2)
+        (= (tamano r6) 4)
+        (= (tamano r7) 1)
+        (= (tamano r8) 4)
+
+        (= (tamano r11) 3)
+        (= (tamano r22) 4)
+        (= (tamano r33) 1)
+        (= (tamano r44) 2)
+        (= (tamano r55) 1)
+        (= (tamano r66) 3)
+        (= (tamano r77) 2)
+        (= (tamano r88) 3)    
+    )
+
+    (:goal 
+        (or (forall (?res - reserva) (or (visitada ?res)))))
+    ;(:metric maximize (cantidad_reservas))
+    ;(:metric maximize (xctj_ocupacion))
+    (:metric maximize (+ (/ (xctj_ocupacion) (cantidad_reservas)) (/ (cantidad_reservas) 5)))
+    ;(:metric maximize (+ (* (/ (xctj_ocupacion) (cantidad_reservas)) 0.5) (* (cantidad_reservas) 0.5)))
+    ;(:metric minimize (+ (* (- 0 (/ (xctj_ocupacion) (cantidad_reservas))) 0.5) (* (dias_libres) 0.5)))
+    ;(:metric minimize (+ (/ (suma_porcentaje) (cantidad_reservas)) (- (- 5 (reservas_descartadas)) (cantidad_reservas))))
+    ;(:metric minimize (+ (/ (suma_porcentaje) (cantidad_reservas)) (* (/ (dias_libres) 120) 4)))
+    ;;; dias libres esta en funcion de hab * dias
+
 )
--- outputTest.py
+++ outputTest.py
@@ -3,28 +3,15 @@ import sys
 dict = {}
 
 def main():
-    h = int(input('número de habitaciones: '))
-    n = int(input('número de reservas: '))
-    i = 0
-    while i < n:
-        r = input('nombre reserva: ')
-        l = list()
-        j = 0
+    h = input('introduce lista habitaciones: ')
+    lh = h.split(' ') 
 
-        while j < h:
-            l.append(False)
-            j += 1
-        dict[r] = l
-        i += 1
+    #r = input('introduce lista reservas: ')
+    #lr = r.split(' ')
+        
+    for x in lh:
+        dict[x] = set()
     
-    
-
-    print('Initial state:')
-    for key in dict.keys():
-        print(key)
-        print(dict[key])
-
-
     act = ""
     actions = []
     
@@ -40,34 +27,41 @@ def main():
         if 'DESCARTAR' not in str and 'REACH-GOAL' not in str:
             st = str.find(' H')
         
-            st += 2
+            st += 1
             aux = str[st:]
             end = st+aux.find(' ')
 
-            hab = int(str[st:end])
+            hab = str[st:end]
 
             if 'RESERVAR' in str:
+                #print('reservar ') 
                 res = str[end+1:]
-                dict[res.lower()][hab] = True
+                #print(res), print(hab)
+                dict[hab.lower()].add(res.lower())
             elif 'ELIMINAR' in str:
+                #print('eliminar')
                 st = end+1
                 aux = str[st:]
                 end = st+aux.find(' ')
                 
                 res2 = str[end+1:]
-                dict[res2.lower()][hab] = False 
+                dict[hab.lower()].remove(res2.lower())
+                #print(res), print(hab2) 
             elif 'CAMBIO_H' in str:
+                #print('cambio_h')
                 st = end+1
                 aux = str[st:]
                 end = st+aux.find(' ')
                 
-                hab2 = int(str[st+1:end])
+                hab2 = str[st:end]
                 res = str[end+1:]
 
-                dict[res.lower()][hab] = False
-                dict[res.lower()][hab2a] = True
+                dict[hab.lower()].remove(res.lower())
+                dict[hab2.lower()].add(res.lower())
+                #print(res), print(hab), print(hab2) 
 
             elif 'CAMBIO_R' in str:
+                #print('cambio_r')
                 st = end+1
                 aux = str[st:]
                 end = st+aux.find(' ')
@@ -75,16 +69,18 @@ def main():
                 res1 = str[st:end]
                 res2 = str[end+1:]
 
-                dict[res1.lower()][hab] = False
-                dict[res2.lower()][hab] = True
-
+                dict[hab.lower()].remove(res1.lower())
+                dict[hab.lower()].add(res2.lower())
+                #print(res), print(res2), print(hab) 
         i += 1
     
 
     
     for key in dict.keys():
-        print(key)
-        print(dict[key])
+        print(key, end = ':') 
+        for x in dict[key]:
+            print(x, end = ', ')
+        print()
 
 if __name__ == '__main__':
     main()
