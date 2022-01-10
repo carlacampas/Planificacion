56cf6e6ccff6585796e95a29b31471e911459294
laia.valles <laia.valles@estudiantat.upc.edu>
2022-01-10 22:19:21 +0100

    pujar trazas

--- generator/plot.py
+++ generator/plot.py
@@ -1,14 +1,19 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import glob
 
-row = pd.read_csv('timeResults.csv', header=None)
-for i in range(0, len(row)):
-    x = np.arange(1, len(row.iloc[i]) + 1)
-    y = np.array(row.iloc[i])
-    # plotting
-    plt.title("Tiempo de ejecución según el tamaño del problema")
-    plt.xlabel("Tamaño del problema (nº habitaciones)")
-    plt.ylabel("Tiempo de ejecución (s)")
-    plt.plot(x, y, color ="blue")
-    plt.show()
+fileList = glob.glob("*.csv")
+
+for file in fileList:
+    print(file)
+    row = pd.read_csv(file, header=None)
+    for i in range(0, len(row)):
+        x = np.arange(1, len(row.iloc[i]) + 1)
+        y = np.array(row.iloc[i])
+        # plotting
+        plt.title("Extensión " + file[3] + ": Tiempo de ejecución según el tamaño del problema")
+        plt.xlabel("Tamaño del problema (nº habitaciones)")
+        plt.ylabel("Tiempo de ejecución (s)")
+        plt.plot(x, y, color ="blue")
+        plt.show()
